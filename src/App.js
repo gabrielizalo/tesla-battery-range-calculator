81a679595f6252cacd0920ab95f2c7fb8ec532a8
Gabriel Porras <ghporras@gmail.com>
2021-11-24 19:31:32 -0500

    feat: 1.4 Header component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, {Component} from "react";
-import "./App.css"
+
+import "./App.css";
+import Header from "./components/Header";
 
 class App extends Component {
   render() {
     return (
       <div>
-        <h2>Let's get started.</h2>
+        <Header/>
       </div>
     );
   }
---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+import "./Header.css"
+import logoUrl from "../asstets/logo.svg";
+
+function Header() {
+  return (
+    <div className="header">
+      <img src={logoUrl} alt="Tesla"/>
+    </div>
+  );
+}
+
+export default Header;
